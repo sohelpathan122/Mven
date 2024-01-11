44100cbf5f0d1e1be51867b55673281bb1fbf541
sohelpathan122 <sohelpathan5110@gmail.com>
2024-01-10 19:10:47 -0500

    Add Project

---
+++ src/test/java/Login/Amazon.java
@@ -0,0 +1,21 @@
+package Login;
+
+import org.testng.annotations.Test;
+
+public class Amazon {
+  @Test
+  public void Product() 
+  {
+	  System.out.println("Amazon product was good");
+  }
+  @Test
+  public void Defect() 
+  {
+	  System.out.println(15/5);
+  }
+  @Test
+  public void Good() 
+  {
+	  System.out.println(12*4);
+  }
+}
---
+++ src/test/java/Registration/Facebook.java
@@ -0,0 +1,24 @@
+package Registration;
+
+import org.testng.annotations.Test;
+
+public class Facebook 
+{
+  @Test
+  public void Create() 
+  {
+	  int k=12*2;
+	  System.out.println("Create File "+k);
+  }
+  @Test
+  public void Home() 
+  {
+	  String P="Upload,Set profile";
+	  System.out.println("Home Data "+P);
+  }
+  @Test
+  public void Upload() 
+  {
+	  System.out.println("Upload A new Pic Of yoursef");
+  }
+}
